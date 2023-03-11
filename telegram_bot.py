e28d3e678f1cde98046e034642d6269a4dd1c204
KesLinz <99072022+KesLinz@users.noreply.github.com>
2023-03-11 13:56:45 +0300

    Add Tbot.

--- telegram_bot.py
+++ telegram_bot.py
@@ -1,14 +1,35 @@
-import telegram
+import argparse
 import os
+from random import shuffle
+from time import sleep
+
+import telegram
 from dotenv import load_dotenv
+from pytimeparse import parse
 
 
 def main():
     load_dotenv()
     bot = telegram.Bot(token=os.environ['TELEGRAM_BOT_TOKEN'])
     chat_id = os.environ['TELEGRAM_CHAT_ID']
-    with open('images/spacex_4.jpg', 'rb') as photo:
-        bot.send_photo(chat_id=chat_id, photo=photo)
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        'time_interval',
+        type=str,
+        help='Enter the time interval with which the bot will send pictures.',
+        default='4h',
+        nargs='?',
+    )
+    args = parser.parse_args()
+
+    photos = os.listdir('images')
+    while True:
+        shuffle(photos)
+        for rand_photo in photos:
+            with open(os.path.join('images', rand_photo), 'rb') as photo:
+                bot.send_photo(chat_id=chat_id, photo=photo)
+            sleep(parse(args.time_interval))
 
 
 if __name__ == '__main__':
